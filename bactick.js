279218451b35440c70f9616797c5d114719c2cc1
Sami0189 <samisaimon28@gmail.com>
2024-11-14 23:29:15 +0600

    bactick(`) use

---
+++ bactick.js
@@ -0,0 +1,33 @@
+const first='jaan'
+const last = 'pakhi'
+const greet='potas potas';
+
+const name= first + ' '+ last+ ' ' + greet;
+console.log(name); 
+
+const a = 10;
+const b = 20;
+
+const result= 'the sum of ' + a + ' and ' + b + ' is ' + (a+b);
+console.log(result);
+
+const math = `the sum of ${a} and ${b} is ${a+b}`;
+console.log(math);
+
+const email = 'hi john \n'+
+'cena cena laga \n'+ 
+'mara mari korba naki';
+console.log(email);
+
+const challenge = `sami who are
+you, bolo na
+kan, hmm `;
+console.log(challenge);
+
+
+const c = 10;
+const d = 20;
+const numbers=[23,12,54,65];
+const student={name:'sami', age:5}
+const totals =`the sum of ${numbers[1]} and ${student.age} is ${numbers[1]+student.age}`;
+console.log(totals)
